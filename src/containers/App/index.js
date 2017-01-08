914b2f83a86c9b6af0b57d4b452299127e25dfc0
ak110dev <ak.apichan1103@gmail.com>
2017-01-09 00:11:01 +0700

    :art: can change state

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import {
   StyleSheet,
   View,
-  Text
+  Text,
+  Button
 } from 'react-native';
 // import { connect } from 'react-redux';
 
 // import * as actions from './actions';
 
 export default class App extends Component {
+  state = {
+    message: 'Welcome to React Native!'
+  }
+
+  onClick = () => {
+    this.setState({
+      message: 'Hello World!'
+    });
+  }
+  onClick2 = () => {
+    this.setState({
+      message: 'Hi John!'
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          {this.state.message}
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.android.js
@@ -22,6 +38,12 @@ export default class App extends Component {
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
+        <Button onPress={this.onClick}
+          title="Learn More" color="#841584"
+          accessibilityLabel="Learn more about this purple button" />
+        <Button onPress={this.onClick2}
+          title="Learn More" color="#123456"
+          accessibilityLabel="Learn more about this purple button" />
       </View>
     );
   }
